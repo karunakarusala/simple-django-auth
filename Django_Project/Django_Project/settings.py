0c47a6c91aec5abb133f3005e8dc8db5eb4ec50e
Karunakar Usala <karunakarusala@gmail.com>
2024-12-23 17:23:15 +0530

    Update settings.py

--- Django_Project/Django_Project/settings.py
+++ Django_Project/Django_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@43uii-1sm&i^*t8@d3zz(jaz)ln3cu6v5exh##os2k$*(0c1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
